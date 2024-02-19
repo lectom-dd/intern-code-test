01f4468a8571bac218f3f2bb68c369682b2ce7aa
lectom-dd <lectom@dingdinguniv.co.kr>
2024-02-19 10:22:26 +0900

    [ICT-004] 이름 충돌 해결. 없는 리소스 부분 삭제

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,3 @@ COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
 COPY api.py ./
-COPY worker.py ./
--- api.py
+++ api.py
@@ -3,11 +3,9 @@ from pydantic import BaseModel
 from redis import Redis
 from rq import Queue
 
-from worker import runTask
-
 app = FastAPI()
 
-redis_conn = Redis(host='test_redis', port=6379)
+redis_conn = Redis(host='redis_queue', port=6379)
 q = Queue('my_queue', connection=redis_conn)
 
 @app.get('/hello')
